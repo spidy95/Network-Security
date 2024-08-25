9a7c3641c0814aecd53591fea6a4856cff73dea6
spidy95 <atul.spider95@gmail.com>
2024-08-25 16:01:21 +0530

    data ingested and working now

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -34,11 +34,18 @@ from networksecurity.entity.artifact_entity import(
 
 class TrainingPipeline:
     def __init__(self):
-        pass
+        self.training_pipeline_config = TrainingPipelineConfig()
     
     def start_data_ingestion(self):
         try:
-            pass
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Starting Data Ingestion")
+            data_injestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_injestion_artifact=data_injestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_injestion_artifact}")
+            return data_injestion_artifact
+            
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -74,6 +81,7 @@ class TrainingPipeline:
         
     def run_pipeline(self):
         try:
-            pass
+            data_ingestion_artifact = self.start_data_ingestion()
+            print(data_ingestion_artifact)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
